9e7835b6e8efe6ec646463643535dc811fdfbf5f
Shaun Martin <shaun@samsite.ca>
2017-04-19 19:49:39 -0700

    Screen grab testing

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Setup
 
 * [OpenCV](https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_setup/py_table_of_contents_setup/py_table_of_contents_setup.html#py-table-of-content-setup)
+
+# Notes
+
+## Screen grabbing
+* FPS is *not* helped by a reduced resolution or graphics quality settings. This is
+  a CPU-bound workload. My old Phenom II X4 can't keep up with GTA and the grabber as is.
+  SOUNDS LIKE I NEED A 32 THREAD RYZEN CHIP. FOR SURE. TOTALLY JUSTIFIED.
+* Update: It may have been a misbehaving Docker container running in the background.
+  Needs moar testing.
--- gtaselfdriving.py
+++ gtaselfdriving.py
@@ -1,15 +1,27 @@
+from __future__ import print_function
 import numpy as np
 from PIL import ImageGrab as ig
 import cv2
+import time
 
-BBOX = (0,40,1600,1240)
+BBOX=(0,40,1024,808)
 
-while(True):
-  raw = ig.grab(bbox=BBOX)
-  numpied = np.array(raw.getdata(), dtype='uint8').reshape((raw.size[1], raw.size[0], 3))
-  cv2.imshow('window', numpied)
-  
-  # bail on 'q' keypress
-  if cv2.waitKey(25) & 0xFF == ord('q'):
-    cv2.destroyAllWindows()
-    break
+def grabscreen():
+    lt = time.time()
+    while(True):
+      numpied = np.array(ig.grab(bbox=BBOX), dtype='uint8')
+      cv2.imshow('window', cv2.cvtColor(numpied, cv2.COLOR_BGR2RGB))
+      ct = time.time()
+      print(fps(lt, ct))
+      lt = ct
+
+      # bail on 'q' keypress
+      if cv2.waitKey(25) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
+
+def fps(prev_time, cur_time):
+    return round((1 / (cur_time - prev_time)), 2)
+
+if __name__ == '__main__':
+    grabscreen()
